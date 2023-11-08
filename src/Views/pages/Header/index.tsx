7694fd7a646ed230a403faa15c20c45a698047d5
rodrigo_motocasystems <rodrigo@motocasystems.com.br>
2023-11-08 09:59:41 -0300

    Adicionando svgs usado nos cards e implementação dos mesmos

--- src/Views/pages/Header/index.tsx
+++ src/Views/pages/Header/index.tsx
@@ -1,6 +1,8 @@
-import { Container, Card } from "./styles";
+import { Container, Card, CardImageSelected, CardImageChoose } from "./styles";
 
 import exclamationIcon from "../../../assets/Icons/exclamation_icon.svg";
+import libIcon from "../../../assets/Icons/lib_icon.svg";
+import trashIcon from "../../../assets/Icons/trash_icon.svg";
 
 export default function Header() {
   return (
@@ -11,7 +13,34 @@ export default function Header() {
         </div>
 
         <div className="main">
-          <span>add header</span>
+          <div style={{ width: "100%" }}>
+            <div style={{ display: "flex" }}>
+              <img src={exclamationIcon} />
+              <span style={{ fontSize: 10, color: "#06F", marginLeft: 5 }}>
+                Imagem atual
+              </span>
+            </div>
+            <CardImageSelected>
+              <input type="checkbox" checked={true} readOnly />
+
+              <span>image</span>
+
+              <div className="actions">
+                <img src={trashIcon} />
+              </div>
+            </CardImageSelected>
+          </div>
+
+          <div style={{ width: "100%" }}>
+            <CardImageChoose>
+              <img src={libIcon} />
+              <label>
+                <span style={{ fontSize: 11 }}>+ Adicionar imagem</span>
+
+                <input type="file" />
+              </label>
+            </CardImageChoose>
+          </div>
         </div>
       </Card>
     </Container>
--- src/Views/pages/Header/styles.ts
+++ src/Views/pages/Header/styles.ts
@@ -19,5 +19,57 @@ export const Card = styled.div`
   .main {
     width: 100%;
     padding: 20px 30px;
+    gap: 20px;
+
+    display: flex;
+  }
+`;
+
+export const CardImageSelected = styled.div`
+  position: relative;
+  border-radius: 5px;
+  border: 2px solid #06f;
+  margin-top: 8px;
+  height: 150px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  input {
+    position: absolute;
+    top: 8px;
+    left: 8px;
+  }
+
+  .actions {
+    position: absolute;
+    right: 0;
+    height: 100%;
+    background: rgba(232, 232, 232, 0.5);
+    padding: 10px;
+
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const CardImageChoose = styled.div`
+  border-radius: 5px;
+  border: 1px dashed #000;
+  margin-top: 22px;
+  height: 150px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  input {
+    display: none;
+  }
+
+  label {
+    cursor: pointer;
   }
 `;
